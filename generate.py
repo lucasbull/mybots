21703f453abffd077d614a9c49fea7e1d8c0d0ea
Lucas Bull <lucas.bull@gmail.com>
2021-02-11 14:25:07 -0500

    Added many links (boxes) to create a tower.

--- generate.py
+++ generate.py
@@ -1,9 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-length = 1
-width = 2
-height = 3
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
+for x_pos in range(6):
+	x = x_pos - 3
+	for z_pos in range(6):
+		z = z_pos - 3
+		length = 1
+		width = 1
+		height = 1
+		y = 0.5
+		for count in range(10):
+			pyrosim.Send_Cube(name="Box", pos=[x,z,y] , size=[length,width,height])
+			y = y + 0.45*height + 0.5*height
+			length = 0.9 * length
+			width = 0.9 * width
+			height = 0.9 * height
 pyrosim.End()
