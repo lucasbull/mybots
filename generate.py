7eea72e74277cea93300bd2d4217147fcee412db
Lucas Bull <lucas.bull@gmail.com>
2021-02-12 15:19:20 -0500

    Added torso, frontleg, and backleg with joints.

--- generate.py
+++ generate.py
@@ -5,27 +5,23 @@ def main():
 	Create_Robot()
 
 def Create_World():
-	x_pos = -3
-	z_pos = 3
-	y_pos = 0.5
-	length = 1
-	width = 1
-	height = 1
 	pyrosim.Start_SDF("world.sdf")
-	pyrosim.Send_Cube(name="Box", pos=[x_pos,z_pos,y_pos] ,
-		size=[length,height,width])
+	pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] ,
+		size=[1,1,1])
 	pyrosim.End()
 
 def Create_Robot():
-	x_pos = 0
-	z_pos = 0
-	y_pos = 0.5
-	length = 1
-	width = 1
-	height = 1
 	pyrosim.Start_URDF("body.urdf")
-	pyrosim.Send_Cube(name="Torso", pos=[x_pos,z_pos,y_pos] ,
-		size=[length,height,width])
+	pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] ,
+		size=[1,1,1])
+	pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , 
+		child = "BackLeg" , type = "revolute", position = "1 0 1")
+	pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] ,
+		size=[1,1,1])
+	pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , 
+		child = "FrontLeg" , type = "revolute", position = "2 0 1")
+	pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] ,
+		size=[1,1,1])
 	pyrosim.End()
 
 main()
