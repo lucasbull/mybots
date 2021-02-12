278443dedba814186fc78b350a4497e0553cbc0c
Lucas Bull <lucas.bull@gmail.com>
2021-02-12 15:00:00 -0500

    Created world and robot in our simulation.

--- generate.py
+++ generate.py
@@ -1,5 +1,31 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-pyrosim.End()
+def main():
+	Create_World()
+	Create_Robot()
+
+def Create_World():
+	x_pos = -3
+	z_pos = 3
+	y_pos = 0.5
+	length = 1
+	width = 1
+	height = 1
+	pyrosim.Start_SDF("world.sdf")
+	pyrosim.Send_Cube(name="Box", pos=[x_pos,z_pos,y_pos] ,
+		size=[length,height,width])
+	pyrosim.End()
+
+def Create_Robot():
+	x_pos = 0
+	z_pos = 0
+	y_pos = 0.5
+	length = 1
+	width = 1
+	height = 1
+	pyrosim.Start_URDF("body.urdf")
+	pyrosim.Send_Cube(name="Torso", pos=[x_pos,z_pos,y_pos] ,
+		size=[length,height,width])
+	pyrosim.End()
+
+main()
--- simulate.py
+++ simulate.py
@@ -8,6 +8,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 for x in range(1000):
