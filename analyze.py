1bbffeccf6f1b5d9029038c471949c242c83326f
Lucas Bull <lucas.bull@gmail.com>
2021-02-18 17:43:06 -0500

    Last commit should have been front leg.

--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as mp
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 
+
 mp.plot(backLegSensorValues, "black", label="Back Leg Sensor Values", linewidth="3")
 mp.plot(frontLegSensorValues, "red", label="Front Leg Sensor Values")
 mp.legend()
