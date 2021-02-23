aa2dfc50e4998639d2a669009461791f1bada44c
Lucas Bull <lucas.bull@gmail.com>
2021-02-23 11:24:01 -0500

    Added sin functions to control the motors.

--- analyze.py
+++ analyze.py
@@ -3,9 +3,13 @@ import matplotlib.pyplot as mp
 
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
+targetAnglesValues = numpy.load('data/targetAngles.npy')
+print(targetAnglesValues)
 
 
-mp.plot(backLegSensorValues, "black", label="Back Leg Sensor Values", linewidth="3")
-mp.plot(frontLegSensorValues, "red", label="Front Leg Sensor Values")
-mp.legend()
+#mp.plot(backLegSensorValues, "black", label="Back Leg Sensor Values", linewidth="3")
+#mp.plot(frontLegSensorValues, "red", label="Front Leg Sensor Values")
+mp.plot(targetAnglesValues)
+
+#mp.legend()
 mp.show()
--- simulate.py
+++ simulate.py
@@ -3,13 +3,10 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
-import math
 import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
-pi = math.pi
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
@@ -21,15 +18,17 @@ pyrosim.Prepare_To_Simulate("body.urdf")
 
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
+targetAngles = numpy.pi/4*numpy.sin(numpy.linspace(-numpy.pi, numpy.pi, 1000))
+#numpy.save('data/targetAngles', targetAngles)
 
 for x in range(1000):
    p.stepSimulation()
    backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
    frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = "Torso_BackLeg", 
-   controlMode = p.POSITION_CONTROL, targetPosition = random.random()*pi-pi/2, maxForce = 50)
+   controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[x], maxForce = 50)
    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = "Torso_FrontLeg", 
-   controlMode = p.POSITION_CONTROL, targetPosition = random.random()*pi-pi/2, maxForce = 50)
+   controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[x], maxForce = 50)
    time.sleep(1/60)
 numpy.save('data/backLegSensorValues', backLegSensorValues)
 numpy.save('data/frontLegSensorValues', frontLegSensorValues)
