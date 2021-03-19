409b689c21606efbca3f6638bf3c7f4d67050c9c
Lucas Bull <lucas.bull@gmail.com>
2021-03-19 16:22:27 -0400

    Finished random search!

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 def main():
 	Generate_World()
@@ -32,12 +33,14 @@ def Generate_Brain():
 	pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
 	pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
 	pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-	pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -.5 )
-	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -.5 )
-	pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 3 , weight = 0 )
-	pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -.5 )
-	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = 0 )
-	pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -.5 )
+
+	for sensorneuron in range(3):
+
+
+		for motorneuron in range(3, 5):
+
+				pyrosim.Send_Synapse( sourceNeuronName = sensorneuron , targetNeuronName = motorneuron , weight = random.randrange(-1, 1) )
+
 	pyrosim.End()
 
 main()
---
+++ search.py
@@ -0,0 +1,5 @@
+import os
+
+for x in range(5):
+	os.system("python generate.py")
+	os.system("python simulate.py")
