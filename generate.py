23f4f2a4da95ee9d68691aecd654f7d26eb7ea56
Lucas Bull <lucas.bull@gmail.com>
2021-04-19 15:28:13 -0400

    Played with quaternions.

--- generate.py
+++ generate.py
@@ -1,5 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] , size=[1,1,1])
+pyrosim.End()
+
+
+pyrosim.Start_URDF("body.urdf")
+pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] ,
+	size=[1,1,1])
+pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , 
+	child = "BackLeg" , type = "revolute", position = "1 0 1")
+pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] ,
+	size=[1,1,1])
+pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , 
+	child = "FrontLeg" , type = "revolute", position = "2 0 1")
+pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] ,
+	size=[1,1,1])
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
 import pybullet as p
 import time
 import pybullet_data
+import os
+
+os.system("python generate.py")
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -9,9 +12,12 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
+robot = p.loadURDF("body.urdf")
+
 
 
 for x in range(1000):
    p.stepSimulation()
    time.sleep(1/60)
+   print(p.getLinkState(robot, 0))
 p.disconnect()
