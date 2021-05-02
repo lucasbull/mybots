b3086dba8c0ab4b5f76c9fa2cd4c9826154e49bc
Lucas Bull <lucas.bull@gmail.com>
2021-05-02 14:29:01 -0400

    Changed number of possible mutations between generations from 1-6 to 10-30.

--- solution.py
+++ solution.py
@@ -246,7 +246,7 @@ class SOLUTION:
 		else:
 			numMotorNeurons = c.numMotorNeuronsWithoutArms
 
-		numMutations = random.randint(1,6)
+		numMutations = random.randint(10,30)
 
 		for mutation in range(numMutations):
 			selectArray = random.randint(0,3)
